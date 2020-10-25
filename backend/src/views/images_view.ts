566c58676b9037508fc085fc0e28dc02000242a9
AndreTorquato <dede100cmts123@gmail.com>
2020-10-25 17:52:59 -0300

    day 2

---
+++ backend/src/views/images_view.ts
@@ -0,0 +1,14 @@
+import Image from "../models/image";
+
+export default {
+    render(image: Image){
+    return {
+            id: image.id,
+            path: `http://localhost:3333/uploads/${image.path}`,
+        }
+    },
+    renderMany(images: Image[]){
+        return images.map(image => this.render(image));
+    }
+};
+ 
